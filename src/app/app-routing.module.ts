6d335b1897edf90e3cd6916778e9fcecd46d025c
Juan Cuero <juanromancuero@gmail.com>
2026-03-17 14:55:18 -0500

    Configurar AppModule con HttpClient y routing principal por roles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard, ChoferGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./paginas/login/login.page.module').then(m => m.LoginPageModule)
+  }, 
+  {
+    path: 'inicio', 
+    loadChildren: () => import('./paginas/inicio/inicio.page.module').then(m => m.InicioPageModule),
+    canActivate: [ChoferGuard] 
+  },  
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
