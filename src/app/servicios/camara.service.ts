5a81e3caf6961227b2e6865412d3e953ec6981ec
Juan Cuero <juanromancuero@gmail.com>
2026-05-17 15:48:48 -0500

    Actualizar calidad de imagen en el servicio de cámara

--- src/app/servicios/camara.service.ts
+++ src/app/servicios/camara.service.ts
@@ -9,13 +9,13 @@ export class CamaraService {
   async tomarFoto(): Promise<string | null> {
     try {
       const foto = await Camera.getPhoto({
-        quality: 10,
+        quality: 60,
         allowEditing: false,
         resultType: CameraResultType.Base64,
         source: CameraSource.Camera,
         saveToGallery: false,
         width: 512,
-        height: 512
+        height: 512,
       });
       return foto.base64String || null;
     } catch (err) {
