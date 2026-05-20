3835c0496488645421ee9c232062de569db41c9a
Juan Cuero <juanromancuero@gmail.com>
2026-05-19 21:30:48 -0500

    Refactor rutas en app-routing.module.ts para mejorar la legibilidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,44 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard, ChoferGuard } from './guards/auth.guard';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard, ChoferGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
   },
   {
-    path: 'login',
-    loadChildren: () => import('./paginas/login/login.page.module').then(m => m.LoginPageModule)
-  }, 
+    path: "login",
+    loadChildren: () =>
+      import("./paginas/login/login.page.module").then(
+        (m) => m.LoginPageModule,
+      ),
+  },
   {
-    path: 'inicio', 
-    loadChildren: () => import('./paginas/inicio/inicio.page.module').then(m => m.InicioPageModule),
-    canActivate: [ChoferGuard] 
-  },  
+    path: "inicio",
+    loadChildren: () =>
+      import("./paginas/inicio/inicio.page.module").then(
+        (m) => m.InicioPageModule,
+      ),
+    canActivate: [ChoferGuard],
+  },
   {
-    path: '**',
-    redirectTo: 'login'
-  }
+    path: "mapa/:rutaId",
+    loadChildren: () =>
+      import("./paginas/mapa/mapa.page.module").then((m) => m.MapaPageModule),
+    canActivate: [ChoferGuard],
+  },
+  {
+    path: "**",
+    redirectTo: "login",
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
